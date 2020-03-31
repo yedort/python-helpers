96fddc3aa5e6b3f4f88d82801963a58447ca3ce8
Yalcin Erdemir <yedort@hotmail.com>
2020-03-31 18:54:06 +0400

    Update selenium.py

--- selenium.py
+++ selenium.py
@@ -9,53 +9,14 @@ try:
     from selenium.webdriver.support import expected_conditions as EC
     from selenium.webdriver.common.keys import Keys
     from selenium.common.exceptions import WebDriverException, NoSuchElementException, NoSuchAttributeException
-    from os import path
-    from glob import glob
     from platform import system as os_system
-    from time import sleep, time
+    from time import sleep
     from subprocess import run
     from tempfile import gettempdir
-    from urllib.request import urlopen
-    from urllib.error import URLError, HTTPError
-    import re, random
 except ImportError as e:
     pass
     print('Python package error: ' + str(e).strip('Message: '))
     exit()
-
-# HELPERS
-
-def unix_time():
-    return int(round(time() * 1000))
-            
-def regex_match(pattern, string, group=0):
-    try:
-        return re.search(pattern, string).group(group)
-    except:
-        pass
-        return None
-
-def get_file(filename, split_by_lines=False):
-    with open(path.abspath(filename), 'r', encoding='utf-8') as fh:
-        file_contents = fh.read()
-    if split_by_lines:
-        file_contents = file_contents.strip().split('\n')
-    return file_contents
-
-def write_file(filename, data, append=False):
-    mode = 'a' if append else 'w'
-    with open(path.abspath(filename), mode, encoding='utf-8') as fh:
-        fh.write(data)
-
-def fetch_url(url):
-    try:
-        content = urlopen(url).read().decode('utf-8')
-        return content
-    except (URLError, HTTPError):
-        pass
-        return None
-
-# FOR SELENIUM
     
 def go_to(driver, url, tab_action=False):
     if driver.current_url != 'about:blank':
