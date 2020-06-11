89dc6eb6c3074ff9c51d12975a7c668db0726d53
Yalcin Erdemir <yedort@hotmail.com>
2020-06-11 13:39:34 +0400

    Update yedortselenium.py

--- yedortselenium.py
+++ yedortselenium.py
@@ -11,7 +11,7 @@ try:
     from selenium.webdriver.common.keys import Keys
     from selenium.common.exceptions import WebDriverException, NoSuchElementException, NoSuchAttributeException
     from time import sleep
-    from inc.yedorthelpers import YedortHelpers as helpers
+    from yedorthelpers import YedortHelpers as helpers
 except ImportError as e:
     pass
     print('Python package error: ' + str(e).strip('Message: '))
